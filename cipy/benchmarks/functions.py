1d1452c0f144762aa8cff98a23ffb902daf42796
avanwyk <abvanwyk@gmail.com>
2016-07-27 23:25:36 +0200

    Add additional benchmark functions
    Add Ackley, Rosenbrock and H1 functions.

--- cipy/benchmarks/functions.py
+++ cipy/benchmarks/functions.py
@@ -17,10 +17,26 @@
 import numpy as np
 
 
-def sphere(x):
-    return np.sum(x * x)
+def ackley(x):
+    A = -0.2 * np.sqrt(0.5 * (x[0] ** 2 + x[1] ** 2))
+    B = 0.5 * (np.cos(2. * np.pi * x[0]) + np.cos(2. * np.pi * x[1]))
+    return -20. * np.exp(A) - np.exp(B) + 20. + np.e
+
+
+def h1(x):
+    A = (np.sin(x[0] - x[1] / 8))**2 + (np.sin(x[1] + x[0] / 8))**2
+    B = ((x[0] - 8.6998)**2 + (x[1] - 6.7665)**2)**0.5 + 1
+    return A / B
 
 
 def rastrigin(x):
     a = np.int32(10)
     return a * len(x) + np.sum(x * x - a * np.cos(2 * np.pi * x))
+
+
+def rosenbrock(x):
+    return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
+
+
+def sphere(x):
+    return np.sum(x * x)
